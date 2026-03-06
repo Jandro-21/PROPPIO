66c226586cd8b5795f35e0a43f8a20202f8a400a
Jandro-21 <alejandrodurillovargas21@gmail.com>
2026-03-06 21:51:37 +0100

    Actualizar Tienda.js

--- proppio/src/components/Tienda.js
+++ proppio/src/components/Tienda.js
@@ -13,7 +13,7 @@ export default function Tienda() {
       precio: '17.99€', 
       // Añadimos un array de imágenes para el carrusel
       imgs: [
-        '../JapanDropFront.png',
+        'PROPPIO./JapanDropFront.png',
         '../JapanDropBack.png'
       ],
       descripcion: 'Algodón 100% orgánico con bordado de alta densidad.'
